7a049097bf6c5e9597b6049c7079fc86eb4eb40e
andersongil <andersongil.dev@gmail.com>
2024-09-26 16:27:01 -0400

    WIP: Added cors to the app, follow lint warnings

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
-const routes = require("./routes/index");
-const swaggerDocument = require("./swagger-output.json");
+const swaggerDocument = require("./swagger-output-modified.json");
 
+const routes = require("./routes/index");
 const mongodb = require("./db/db");
 
 const port = 3000;
 const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    next();
-  })
-  .use("/", routes)
-  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.json()).use((req, res, next) => {
+  if (res.headersSent) {
+    console.log("Headers already sent");
+    return next();
+  }
+  next();
+});
+app.use(cors());
+app.use("/", routes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-mongodb.initDb((err, mongodb) => {
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
